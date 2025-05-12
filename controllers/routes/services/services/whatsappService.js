2c0187eec3dcd5e87484115b2b5640b14b3af047
agendafacilpremium <nannymeuamor16@gmail.com>
2025-05-11 23:13:58 -0300

    Create whatsappService.js

---
+++ controllers/routes/services/services/whatsappService.js
@@ -0,0 +1,4 @@
+exports.enviarMensagem = (telefone, mensagem) => {
+  console.log(`(simulação) Enviando mensagem para ${telefone}: ${mensagem}`);
+  // Aqui será feita a integração real com a API do WhatsApp
+};
